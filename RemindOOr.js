0b40d72c277c72b3cb0cac6e17931afa81721b10
satwik-060 <perisetla.srisatwik00@gmail.com>
2021-10-23 12:33:02 +0530

    added title feature
    added title so that every task has its title and its own description

--- RemindOOr.js
+++ RemindOOr.js
@@ -1,75 +1,71 @@
 console.log("welcome to RemindOOr");
 showNotes();
 let addBtn = document.getElementById("addNote");
-addBtn.addEventListener("click", (e)=>{
-    let addText = document.getElementById('remind');
-    let notes = localStorage.getItem("notes");
-    if(notes == null){
-        notesObj = [];
-    }
-    else
-    {
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.push(addText.value);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    addText.value = "";
-    showNotes();
-})
+addBtn.addEventListener("click", (e) => {
+  let addText = document.getElementById("remind");
+  let addTextTitle = document.getElementById("titleOfNote");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let pushObj = { title: addTextTitle.value, notes: addText.value };
+  notesObj.push(pushObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addText.value = "";
+  addTextTitle.value = "";
+  showNotes();
+});
 
-function showNotes(){
-    let notes = localStorage.getItem("notes");
-    if(notes == null){
-        noteTextArr = [];
-    }else{
-        noteTextArr = JSON.parse(notes);
-    }
-    let html = "";
-    let cardContainer = document.getElementById("cardContainer");
-    noteTextArr.forEach(function(element,index){
-        html += `
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    noteTextArr = [];
+  } else {
+    noteTextArr = JSON.parse(notes);
+  }
+  let html = "";
+  let cardContainer = document.getElementById("cardContainer");
+  noteTextArr.forEach(function (element, index) {
+    html += `
         <div class="note">
-                <h4>Note ${index+1}</h4>
+                <h4>${element.title}</h4>
                 <hr>
-                <p>${element}</p>
+                <p>${element.notes}</p>
                 <button class="deleteNote" onclick="deleteNode(this.id)" id = "${index}" >Delete Note</button>
-            </div>  `
-    })
-    if(noteTextArr.length != 0){
-        cardContainer.innerHTML = html;
-    }
-    else{
-        cardContainer.innerHTML = `No Reminder Notes Yet! Type something in the "add a note" section and click the button "Add note" to add a reminder`;
-    }
+            </div>  `;
+  });
+  if (noteTextArr.length != 0) {
+    cardContainer.innerHTML = html;
+  } else {
+    cardContainer.innerHTML = `No Reminder Notes Yet! Type something in the "add a note" section and click the button "Add note" to add a reminder`;
+  }
 }
 
-function deleteNode(index){
-    let notes = localStorage.getItem("notes");
-    if(notes == null)
-    {
-        notesObj = [];
-    }
-    else
-    {
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.splice(index,1);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
-    showNotes();
+function deleteNode(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
 }
 let search = document.getElementById("searchbar");
-search.addEventListener("input" , ()=>{
-    let inputValue = search.value.toLowerCase();
-    let cards = document.getElementsByClassName("note");
-    Array.from(cards).forEach(element =>{
-        let cardTxt = element.getElementsByTagName('p')[0].innerText;
-        if(cardTxt.includes(inputValue)){
-            element.style.display = "grid";
-            console.log("included");
-        }
-        else{
-            element.style.display = "none";
-        }
-    })
-
-})
+search.addEventListener("input", () => {
+  let inputValue = search.value.toLowerCase();
+  let cards = document.getElementsByClassName("note");
+  Array.from(cards).forEach((element) => {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    let cardTitle = element.getElementsByTagName("h4")[0].innerText;
+    if (cardTxt.includes(inputValue) || cardTitle.includes(inputValue)) {
+      element.style.display = "grid";
+      console.log("included");
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
